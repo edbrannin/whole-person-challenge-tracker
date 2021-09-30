533876112084d8f45c08b2ce64a53346943ec9cd
Ed Brannin <edbrannin@gmail.com>
2021-09-30 10:43:11 -0400

    Update days-remaining count every hour

--- src/Tracker.js
+++ src/Tracker.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import useTracker from './useTracker';
 import { differenceInCalendarDays } from 'date-fns'
 import useWindowSize from 'react-use/lib/useWindowSize'
@@ -6,6 +7,9 @@ import Confetti from 'react-confetti'
 const CHALLENGE_START_DAY = new Date('2021-09-07');
 const CHALLENGE_END_DAY = new Date('2021-11-06');
 const CHALLENGE_LENGTH_DAYS = differenceInCalendarDays(CHALLENGE_END_DAY, CHALLENGE_START_DAY);
+
+// Update the number of days remaining every hour
+const DAYS_LEFT_INTERVAL_MILLIS = 60 * 60 * 1000;
 
 const Tracker = () => {
   const {
@@ -18,7 +22,16 @@ const Tracker = () => {
 
   const { width, height } = useWindowSize()
 
-  const daysRemaining = differenceInCalendarDays(CHALLENGE_END_DAY, new Date());
+  const getDaysRemaining = () => differenceInCalendarDays(CHALLENGE_END_DAY, new Date());
+  const [daysRemaining, setDaysRemaining] = useState(getDaysRemaining());
+  useEffect(() => {
+    const interval = setInterval(
+      () => setDaysRemaining(getDaysRemaining()),
+      DAYS_LEFT_INTERVAL_MILLIS,
+    )
+    return () => clearInterval(interval);
+  });
+
   const challengePercent = Math.round((CHALLENGE_LENGTH_DAYS - daysRemaining) / CHALLENGE_LENGTH_DAYS * 1000) / 10;
 
   return (
