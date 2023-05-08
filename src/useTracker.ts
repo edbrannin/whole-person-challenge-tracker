0eeb30aec8aa2ba73659cfcad3fbd3080e4a9ecf
Ed Brannin <edbrannin@gmail.com>
2023-05-08 10:04:32 -0400

    Oh, come ON

--- src/useTracker.ts
+++ src/useTracker.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useEffectOnce } from 'react-use';
 import createPersistedState from 'use-persisted-state';
 
